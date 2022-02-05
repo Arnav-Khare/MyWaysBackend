87ccb9ae2a72ee8b4dc0afbb1a4b8bd76414525b
Arnav-Khare <55575931+Arnav-Khare@users.noreply.github.com>
2022-02-05 11:11:46 +0530

    Delete server directory

--- server/src/db/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const DB = 'mongodb+srv://HashiraFlame:mohil%4051@cluster0.pvkn7.mongodb.net/Users?retryWrites=true&w=majority'
-mongoose.connect(DB).then(()=>{
-    console.log("Connection Successful")
-}).catch((e)=>{
-    console.log(e);
-})
